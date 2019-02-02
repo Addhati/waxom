ac0a2338a6308f9af50cd44cb7373bd2031c4f68
Addhati <addhati@gmail.com>
2019-02-02 18:18:27 +0300

    End

--- main/header/header.js/header-slider.js
+++ main/header/header.js/header-slider.js
@@ -1,16 +1,30 @@
 var buttonLeft = document.querySelector('.arrow-left')
 var buttonRight = document.querySelector('.arrow-right')
 
-buttonRight.onclick = plus;
-buttonLeft.onclick = minus;
+buttonRight.addEventListener('click', plus);
+buttonLeft.addEventListener('click', minus);
+
+var sliders = {
+    img_1: 'green',
+    img_2: 'blue',
+    img_3: 'url(main/header/header-img/header.jpg)'
+}
 
 function plus() {
     var picture = document.querySelector('.header')
-    picture.style.background = 'url(main/header/header-img/header-2.jpg)';
+    picture.style.background = sliders.img_2;
+    picture.classList.add('img_2');
 
 }
 
 function minus() {
     var picture = document.querySelector('.header');
-    picture.style.background = 'url(main/header/header-img/header.jpg)';
+    picture.style.background = sliders.img_1;
+    picture.classList.add('img_1');
+}
+
+function newWindow() {
+
+    var newWin = window.open("about:blank", "hello", "width=200,height=200");
+    newWin.document.write("Привет, мир!");
 }
